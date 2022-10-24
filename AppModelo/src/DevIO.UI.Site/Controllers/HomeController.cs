dc6c2be00e52d50a957823d6c400d1cd93a69641
Marcelo Amador <marcelojaf@gmail.com>
2022-10-24 15:27:40 -0300

    AppModelo funcionando

---
+++ AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ AppModelo/src/DevIO.UI.Site/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
