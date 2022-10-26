8e1382832180aec81384ca0047eb313602f905d4
Marcelo Amador <marcelo@redbitdev.com>
2022-10-26 12:19:49 -0300

    Projeto MVC basico

---
+++ AppMvcBasica/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
